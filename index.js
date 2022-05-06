61ed59a59821c6a603c936cc32a5908e6d1cd732
habulCoderJohnny <fokruljohnny@gmail.com>
2022-05-06 22:02:31 +0600

    server connect to DB | Sent own inserted data to client side

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion} = require('mongodb');
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -10,11 +11,33 @@ app.use(cors());
 app.use(express.json());
 
 
+//CONNECTION
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9khll.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+async function run () {
+    try{
+      await client.connect();
+      const productCollection = client.db("Motor_mania").collection("Bikes")
 
+     //SENT DATA OF PRODUCT
+      app.get('/bikes', async (req, res) => {
+        const query = {};
+        const cursor = productCollection.find(query);
+        const bikes = await cursor.toArray();
+        res.send(bikes);
+    });
+   
+      
 
-
+    } 
+    finally{}
+  } 
+  
+  // call the async finc
+  run().catch(console.dir);
+  console.log('MOTOR MANIA now CONNECTED');
 
 //log 
 app.get('/', (req,res)=>{
